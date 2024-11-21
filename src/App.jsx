03a7a23336d108c26b39176f4e768d1e547c0b1e
DailyScrum <daily.scrum.2000@gmail.com>
2024-11-21 19:56:15 +0100

    dz-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-import Menu from "./components/PizzaMenu";
+import PizzaMenu from "./components/PizzaMenu";
 import "./css/app.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Menu />
+      <PizzaMenu />
     </>
   );
 }

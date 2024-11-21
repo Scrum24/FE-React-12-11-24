0937c44f5ed792695301965ec5c5db255ad0a87a
DailyScrum <daily.scrum.2000@gmail.com>
2024-11-21 19:47:03 +0100

    dz-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-import Main from "./components/Main";
+import Menu from "./components/PizzaMenu";
 import "./css/app.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Menu />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,12 @@ function Header() {
   return (
     <header>
       <div className="logo">PIZZA DAY</div>
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search for the order #"
+      />
+      <div className="username">VLAD</div>
     </header>
   );
 }

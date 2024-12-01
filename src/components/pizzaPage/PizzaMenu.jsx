e684be103a4ddd672935d346b59c9fce2b2c9186
DailyScrum <daily.scrum.2000@gmail.com>
2024-12-01 15:37:30 +0100

    Added useEffect

--- src/components/pizzaPage/PizzaMenu.jsx
+++ src/components/pizzaPage/PizzaMenu.jsx
@@ -1,8 +1,32 @@
 import PizzaMenuItem from "./PizzaMenuItem.jsx";
 import {pizzas} from "./pizzas.js";
 import "./pizza-page.css";
+import {useEffect, useState} from "react";
 
 function PizzaMenu() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    const defaultPizzaList = async () => {
+      try {
+        const res = await fetch(
+          "https://react-fast-pizza-api.onrender.com/api/menu"
+        );
+
+        if (res.status !== 200) {
+          throw new Error("Get request, response is not ok.");
+        }
+
+        const data = await res.json();
+        setPizzas(data.data);
+      } catch (e) {
+        console.log("Get defaultPizzaList error:", e);
+      }
+    };
+
+    defaultPizzaList();
+  }, []);
+
   return (
     <div className="menu-container">
       {pizzas.map((pizzainfo) => (

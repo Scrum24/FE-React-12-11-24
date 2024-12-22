9c85988a8c80441fa0bddcc261f24ae4a4052134
DailyScrum <daily.scrum.2000@gmail.com>
2024-12-23 00:50:25 +0100

    Added useFetch hook (hw 14)

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+
+        const res = await fetch(url);
+
+        if (!res.ok) {
+          throw new Error("Fetch error happened");
+        }
+
+        const data = await res.json();
+        setData(data);
+      } catch (e) {
+        console.log(e.message);
+        setError(e.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return [data, error, isLoading];
+};
+
+export default useFetch;
